9dd3a540a8273bb29a74e94509b60cff14c86081
xiang lei <xiang_lei@venusgroup.com.cn>
2021-01-13 17:50:44 +0800

    update:新增dhttp

--- net/dhttplib/httplib.go
+++ net/dhttplib/httplib.go
@@ -1,34 +1,3 @@
-// Copyright 2014 Gd Author. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package httplib is used as http.Client
-// Usage:
-//
-// import "github.com/astaxie/Gd/httplib"
-//
-//	b := httplib.Post("http://Gd.me/")
-//	b.Param("username","astaxie")
-//	b.Param("password","123456")
-//	b.PostFile("uploadfile1", "httplib.pdf")
-//	b.PostFile("uploadfile2", "httplib.txt")
-//	str, err := b.String()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	fmt.Println(str)
-//
-//  more docs http://Gd.me/docs/module/httplib.md
 package httplib
 
 import (
